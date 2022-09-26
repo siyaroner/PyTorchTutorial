a529504e5c325e17b1796f847e12de3a14562fc6
Şiyar Öner <siyaroner@gmail.com>
2022-09-26 18:43:15 +0300

    grad is auto others manually

--- GradientDecentWithAgutograd&Backpropagation.py
+++ GradientDecentWithAgutograd&Backpropagation.py
@@ -21,7 +21,8 @@
 # Gradients Computation : Autograd
 # Loss Computation      : Pytorch Loss
 # Parameter Updates     : pytorch Optimizer
-
+#######################################################
+"""
 # Number 1
 import numpy as np
 
@@ -47,7 +48,7 @@ print(f"Prediction before training: f(5)= {forward(5):.3f}")
 
 #training
 learning_rate=0.01
-n_iters=10
+n_iters=20
 for epoch in range(n_iters):
     #prediction=forward pass
     y_pred=forward(x)
@@ -62,6 +63,49 @@ for epoch in range(n_iters):
     w-=learning_rate*dw
     
     if epoch%1==0:
+        print(f"epoch {epoch+1}: w={w:.3f}, loss={l:.8f}")
+        
+print(f"Prediction after training f(5)={forward(5):.3f}")
+"""
+#######################################################
+# Number 2
+import torch
+
+#f=w*x
+#f=2*x
+x=torch.tensor([1,2,3,4],dtype=torch.float32)
+y=torch.tensor([2,4,6,8],dtype=torch.float32)
+w=torch.tensor(0.0,dtype=torch.float32,requires_grad=True)
+
+#model prediction
+def forward(x): 
+    return w*x
+#loss
+def loss (y,y_predicted):
+    return ((y_predicted-y)**2).mean()
+
+print(f"Prediction before training: f(5)= {forward(5):.3f}")
+
+#training
+learning_rate=0.01
+n_iters=70
+for epoch in range(n_iters):
+    #prediction=forward pass
+    y_pred=forward(x)
+    
+    #Loss
+    l=loss(y,y_pred)
+    
+    #gradients=backward pass
+    l.backward() #dl/dw
+    
+    #update weights
+    with torch.no_grad():
+        w-=learning_rate*w.grad
+    #zero Gradients
+    w.grad.zero_()
+    
+    if epoch%2==0:
         print(f"epoch {epoch+1}: w={w:.3f}, loss={l:.8f}")
         
 print(f"Prediction after training f(5)={forward(5):.3f}")
