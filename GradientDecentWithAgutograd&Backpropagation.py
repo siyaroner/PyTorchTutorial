8d987fe35a6530c04971df9d94ec4331822b2117
Şiyar Öner <siyaroner@gmail.com>
2022-09-28 18:11:08 +0300

    GradientDecentWithAgutograd&Backpropagation.py is updated

--- GradientDecentWithAgutograd&Backpropagation.py
+++ GradientDecentWithAgutograd&Backpropagation.py
@@ -156,30 +156,47 @@ for epoch in range(n_iters):
 print(f"Prediction after training f(5)={forward(5):.3f}")
 """""
 #######################################################
-# Number 3
+# Number 4
 import torch
 import torch.nn as nn
 #f=w*x
 #f=2*x
-x=torch.tensor([1,2,3,4],dtype=torch.float32)
-y=torch.tensor([2,4,6,8],dtype=torch.float32)
+x=torch.tensor([[1],[2],[3],[4]],dtype=torch.float32)
+y=torch.tensor([[2],[4],[6],[8]],dtype=torch.float32)
 w=torch.tensor(0.0,dtype=torch.float32,requires_grad=True)
 
-#model prediction
-model=nn.Linear()
+x_test=torch.tensor([5],dtype=torch.float32)
+n_spamples,n_features=x.shape
+print(n_spamples,n_features)
 
-print(f"Prediction before training: f(5)= {forward(5):.3f}")
+#model prediction
+input_size=n_features
+output_size=n_features
+model=nn.Linear(input_size,output_size)
+
+class LinearRegresison(nn.Module):
+    def __init__(self,input_dim,output_dim):
+        super(LinearRegresison,self).__init__()
+        #define layers
+        self.lin=nn.Linear(input_dim,output_dim)
+    
+    def forward(self,x):
+        return self.lin(x)
+model=LinearRegresison(input_size,output_size)
+        
+
+print(f"Prediction before training: f(5)= {model(x_test).item():.3f}")
 
 #training and loss
 learning_rate=0.01
-n_iters=70 
+n_iters=100 
 
 loss=nn.MSELoss()
-optimizer=torch.optim.SGD([w],lr=learning_rate)
+optimizer=torch.optim.SGD(model.parameters(),lr=learning_rate)
 
 for epoch in range(n_iters):
     #prediction=forward pass
-    y_pred=forward(x)
+    y_pred=model(x)
     
     #Loss
     l=loss(y,y_pred)
@@ -192,7 +209,8 @@ for epoch in range(n_iters):
     #zero Gradients
     optimizer.zero_grad()
     
-    if epoch%2==0:
-        print(f"epoch {epoch+1}: w={w:.3f}, loss={l:.8f}")
+    if epoch%10==0:
+        [w,b]=model.parameters()
+        print(f"epoch {epoch+1}: w={w[0][0].item():.3f}, loss={l:.8f}")
         
-print(f"Prediction after training f(5)={forward(5):.3f}")
+print(f"Prediction after training f(5)={model(x_test).item():.3f}")
