d65bc60a49339dddacb302c5c24ee67a2ae153e1
Şiyar Öner <siyaroner@gmail.com>
2022-09-28 09:50:28 +0300

    GradientDecentWithAgutograd&Backpropagation.py is updated

--- GradientDecentWithAgutograd&Backpropagation.py
+++ GradientDecentWithAgutograd&Backpropagation.py
@@ -68,6 +68,7 @@ for epoch in range(n_iters):
 print(f"Prediction after training f(5)={forward(5):.3f}")
 """
 #######################################################
+""""
 # Number 2
 import torch
 
@@ -104,6 +105,92 @@ for epoch in range(n_iters):
         w-=learning_rate*w.grad
     #zero Gradients
     w.grad.zero_()
+    
+    if epoch%2==0:
+        print(f"epoch {epoch+1}: w={w:.3f}, loss={l:.8f}")
+        
+print(f"Prediction after training f(5)={forward(5):.3f}")
+"""
+#######################################################
+"""""
+# Number 3
+import torch
+import torch.nn as nn
+#f=w*x
+#f=2*x
+x=torch.tensor([1,2,3,4],dtype=torch.float32)
+y=torch.tensor([2,4,6,8],dtype=torch.float32)
+w=torch.tensor(0.0,dtype=torch.float32,requires_grad=True)
+
+#model prediction
+def forward(x): 
+    return w*x
+
+print(f"Prediction before training: f(5)= {forward(5):.3f}")
+
+#training and loss
+learning_rate=0.01
+n_iters=70
+
+loss=nn.MSELoss()
+optimizer=torch.optim.SGD([w],lr=learning_rate)
+
+for epoch in range(n_iters):
+    #prediction=forward pass
+    y_pred=forward(x)
+    
+    #Loss
+    l=loss(y,y_pred)
+    
+    #gradients=backward pass
+    l.backward() #dl/dw
+    
+    #update weights
+    optimizer.step()
+    #zero Gradients
+    optimizer.zero_grad()
+    
+    if epoch%2==0:
+        print(f"epoch {epoch+1}: w={w:.3f}, loss={l:.8f}")
+        
+print(f"Prediction after training f(5)={forward(5):.3f}")
+"""""
+#######################################################
+# Number 3
+import torch
+import torch.nn as nn
+#f=w*x
+#f=2*x
+x=torch.tensor([1,2,3,4],dtype=torch.float32)
+y=torch.tensor([2,4,6,8],dtype=torch.float32)
+w=torch.tensor(0.0,dtype=torch.float32,requires_grad=True)
+
+#model prediction
+model=nn.Linear()
+
+print(f"Prediction before training: f(5)= {forward(5):.3f}")
+
+#training and loss
+learning_rate=0.01
+n_iters=70 
+
+loss=nn.MSELoss()
+optimizer=torch.optim.SGD([w],lr=learning_rate)
+
+for epoch in range(n_iters):
+    #prediction=forward pass
+    y_pred=forward(x)
+    
+    #Loss
+    l=loss(y,y_pred)
+    
+    #gradients=backward pass
+    l.backward() #dl/dw
+    
+    #update weights
+    optimizer.step()
+    #zero Gradients
+    optimizer.zero_grad()
     
     if epoch%2==0:
         print(f"epoch {epoch+1}: w={w:.3f}, loss={l:.8f}")
