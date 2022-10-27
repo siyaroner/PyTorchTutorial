ba4c4ded72ce5945ab21d5447db73d24f9a25660
Şiyar Öner <siyaroner@gmail.com>
2022-10-27 13:38:45 +0300

    CNN_test; bug fixed

--- CNN_test.py
+++ CNN_test.py
@@ -10,59 +10,114 @@ import numpy as np
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #hyper-parameters
-num_epochs=4
+num_epochs=1
 batch_size=4
 learning_rate=0.001
 
 #dataset has PILImage images of range [0,1].
 #We transform them to Tensor of normalized range [-1,1]
-transform=transforms.Compose(
+transform = transforms.Compose(
     [transforms.ToTensor(),
-     transforms.NOrmalize((0.5,0.5,0.5,0.5),(0.5,0.5,0.5,0.5))])
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-train_dataset=torchvision.datasets.CIFAR10(root=".data/",train=True, download=True,transform=transform)
-test_dataset=torchvision.datasets.CIFAR10(root=".data/",train=False, download=True,transform=transform)
-train_loader=torchvision.datasets.CIFAR10(train_dataset,batch_size=batch_size,shuffle=True)
-test_loader=torchvision.datasets.CIFAR10(test_dataset,batch_size=batch_size,shuffle=False)
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                          shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                         shuffle=False)
+
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# # get some random training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+
+# show images
+# imshow(torchvision.utils.make_grid(images))
+
 
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(3,6,3)
-        self.conv2 = nn.Conv2d(6,9,3)
-        self.conv3 = nn.Conv2d(9,12,3)
-        self.pool=nn.MaxPool2d(2,2)
-        self.conv4 = nn.Conv2d(12,12,3)
-        self.fc1= nn.Linear(1452,10)
-    def forward(self,x):
-        x=self.conv1(x)
-        x=F.relu(x)
-        x=F.relu(self.conv3(F.relu(self.conv2(x))))
-        x=F.relu(self.conv4(self.pool(x)))
-        x=x.view(-1,1452) #Flatten
-        x=self.fc1(x)
-        x=F.softmax(x)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # -> n, 3, 32, 32
+        x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
+        x = x.view(-1, 16 * 5 * 5)            # -> n, 400
+        x = F.relu(self.fc1(x))               # -> n, 120
+        x = F.relu(self.fc2(x))               # -> n, 84
+        x = self.fc3(x)                       # -> n, 10
         return x
+
     
-model=ConvNet()
+model=ConvNet().to(device)
 criterion=nn.CrossEntropyLoss()
 optimizer=torch.optim.SGD(model.parameters(),lr= learning_rate)
-for epoch in range (num_epochs):
-        for i,(images,labels) in enumerate (train_loader):
-            #origin shape: [4,3,32,32]= 4,3,1024
-            #input_layer=3 input channels, 6 output channels, 5 kernel size
-            images=images.to(device)
-            labels=labels.to(device)
-            #forward pass
-            outputs=model(images)
-            loss=criterion(outputs,labels)
-            #backward and optimize
-            optimizer.zero_grad()
-            loss.bakcward()
-            optimizer.step()
-            if i(+1)%2000==0:
-                print(f"index= {i+1} epoch= {epoch+1}/{num_epochs} loss={loss.item():.4f}")
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # origin shape: [4, 3, 32, 32] = 4, 3, 1024
+        # input_layer: 3 input channels, 6 output channels, 5 kernel size
+        images = images.to(device)
+        labels = labels.to(device)
+         # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        #backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if (i+1) % 2000 == 0:
+            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
 print("Traning is finished")
+PATH = './cnn.pth'
+torch.save(model.state_dict(), PATH)
+
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+        
+        for i in range(batch_size):
+            label = labels[i]
+            pred = predicted[i]
+            if (label == pred):
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network: {acc} %')
+
+    for i in range(10):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'Accuracy of {classes[i]}: {acc} %')
