b7e0694c108a9dd88792f25b321d598445e2e523
Şiyar Öner <siyaroner@gmail.com>
2022-11-07 11:13:50 +0300

    activationfunction.py is updated

--- ActivationFunctions.py
+++ ActivationFunctions.py
@@ -2,6 +2,48 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+x = torch.tensor([-1.0, 1.0, 2.0, 3.0])
+
+# # sofmax
+# output = torch.softmax(x, dim=0)
+# print(output)
+# sm = nn.Softmax(dim=0)
+# output = sm(x)
+# print(output)
+
+# # sigmoid 
+# output = torch.sigmoid(x)
+# print(output)
+# s = nn.Sigmoid()
+# output = s(x)
+# print(output)
+
+# #tanh
+# output = torch.tanh(x)
+# print(output)
+# t = nn.Tanh()
+# output = t(x)
+# print(output)
+
+# relu
+# output = torch.relu(x)
+# print(output)
+# relu = nn.ReLU()
+# output = relu(x)
+# print(output)
+
+# leaky relu
+output = F.leaky_relu(x)
+print(output)
+lrelu = nn.LeakyReLU()
+output = lrelu(x)
+print(output)
+
+#nn.ReLU() creates an nn.Module which you can add e.g. to an nn.Sequential model.
+#torch.relu on the other side is just the functional API call to the relu function,
+#so that you can add it e.g. in your forward method yourself.
+
 #option 1 (create nn modules)
 class NeuralNet(nn.Module):
     def __init__(self,input_size,hidden_size):
@@ -18,7 +60,7 @@ class NeuralNet(nn.Module):
         out=self.sigmoid(out)
         return out
 
-#option 3 (use activation functions directluy in forward pass)
+#option 2 (use activation functions directluy in forward pass)
 
 class NeuralNet(nn.Module):
     def __init__(self,input_size,hidden_size):
